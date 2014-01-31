b561a3933c53dabbd50275b07a55be9bc48c2446
Michael Malchak <michaelmalchak@gmail.com>
2014-01-31 10:08:42 -0800

    change ember router path names

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -91,11 +91,11 @@ module.exports = AuthManager;
 var App = require('./app');
 
 App.Router.map(function() {
-	this.resource('sessions', function(){
-		this.route('new');
+	this.resource('sessions', { path: '/profile' }, function(){
+		this.route('new', { path: '/login' });
 	});
 	this.resource('users', function(){
-		this.route('new');
+		this.route('new', { path: '/register' });
 	});
 	this.route('top_secret');
 });
--- public/javascripts/config/routes.js
+++ public/javascripts/config/routes.js
@@ -1,11 +1,11 @@
 var App = require('./app');
 
 App.Router.map(function() {
-	this.resource('sessions', function(){
-		this.route('new');
+	this.resource('sessions', { path: '/profile' }, function(){
+		this.route('new', { path: '/login' });
 	});
 	this.resource('users', function(){
-		this.route('new');
+		this.route('new', { path: '/register' });
 	});
 	this.route('top_secret');
 });
